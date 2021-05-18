8da6597ea1de33b8d8e6370c0cf597bc4f714c30
Hanie20 <damas.johanie@courrier.uqam.ca>
2021-05-18 14:54:18 -0400

    Setup environement

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import logo from './logo.png';
+import Dictionary from './Dictionary';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <img src={logo} className="App-logo"
         alt="logo" />
       </header>
+     <main>
+       <Dictionary />
+        </main>
+        <footer className="App-footer"> 
+        <small > Coded by Johanie Damas </small>
+        </footer>
+      </div>
     </div>
   );
 }
