5d36a183c6bc1f0d56fbb557c507e8b6812fa5f8
Hanie20 <damas.johanie@courrier.uqam.ca>
2021-05-20 08:29:35 -0400

    rebuild

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,26 +3,27 @@ import React, {useState} from "react";
 import "./Dictionary.css";
 
 export default function Dictionary() {
-    let [keyword, setKeyword] = useState("");
+    let [word, setWord] = useState("");
 
-    function handleResponse(response) {
+     function handleResponse(response) {
         console.log(response.data[0]);
     }
 
-    function search(event) {
-        event.preventDefault();
+    function searchWord(event) {
+      event.preventDefault();
         //documentation: https://dictionaryapi.dev/
-        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${word}`;
         axios.get(apiUrl).then(handleResponse);
-    } 
-
-    function manageKeywordChange(event) {
-        setKeyword(event.target.value);
     }
-    return (<div className="Dictionary"> 
-    <form onSubmit = {search}>
-        <input type="search" onChange={manageKeywordChange}/>
-    </form>
-    </div>
-    )
+
+     function manageKeywordChange(event) {
+         setWord(event.target.value);
+    }
+
+ return ( <div className="Dictionary">
+     <form onSubmit= {searchWord}>
+         <input type="search"  onChange={manageKeywordChange}/> 
+          </form>
+ </div>
+ );
 }
