6fca73ed4fde91a5d648e5923a1df396c707e257
Hanie20 <damas.johanie@courrier.uqam.ca>
2021-05-25 23:25:10 -0400

    Add css

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -7,15 +7,26 @@ export default function Dictionary(props) {
     let [word, setWord] = useState(props.defaultKeyword);
     let [results, setResults] = useState(null);
     let [loaded, setLoaded] = useState(false);
+    let [photos, setPhoto] = useState(false)
 
-     function handleResponse(response) {
+     function handleDictionaryResponse(response) {
         setResults(response.data[0]);
+    }
+    function handlePexelsResponse (response) {
+        console.log(response.data);
     }
     function searchWord() {
       //documentation: https://dictionaryapi.dev/
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${word}`;
-        axios.get(apiUrl).then(handleResponse);
+        axios.get(apiUrl).then(handleDictionaryResponse);
+        let pexelsApiKey = `563492ad6f917000010000015952f780e84240f0be7a60c638f5a1fd`;
+        let pexelsApiUrl = ` https://api.pexels.com/v1/search?query=${word}&per_page=1`;
+        let headers = {"Authorization" : `Bearer ${pexelsApiKey}`} ;
+        axios
+        .get(pexelsApiUrl, {  headers: headers}).then
+        (handlePexelsResponse);
     }
+
     function handleSubmit(event) {
       event.preventDefault();
         searchWord();
@@ -51,5 +62,4 @@ export default function Dictionary(props) {
     load();
     return "loading";
 }
- 
 }
--- src/Phonetic.js
+++ src/Phonetic.js
@@ -3,7 +3,7 @@ import "./Phonetic.css";
 export default function Phonetic(props) {
     console.log(props.phonetic);
     return (<div className="Phonetic"> 
-    <a href={props.phonetic.audio} >
+    <a href={props.phonetic.audio} rel="noreferrer" >
         Listen 
     </a>
     <span className="text"> 
